438a6ff88027119af15b54c628d2205425c5219e
Renan Gomes <renan.gomes@live.com>
2020-08-20 06:40:56 -0300

    Added the production URL to wakeup.

--- wakemeup/env.py
+++ wakemeup/env.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 class Environment(BaseSettings):
     endpoints: Set[str] = {
         "https://app-dev-micebot.herokuapp.com",
-        "https://app-micebot.herokuapp.com"
+        "https://micebot-prod.herokuapp.com"
     }
     interval: int = 600  # 10 minutes (time in seconds).
 
